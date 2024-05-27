791b959e2e73fb13ade38d66cfe449f022bcb22f
reddy <reddymaruri9@gmail.com>
2024-05-27 09:43:48 +0530

    coded the caluculator app

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import {useState, useRef} from "react";
+
+function App(){
+
+  const [result,setResult]=useState(0);
+  const inputRef=useRef(null);
+  const resultRef=useRef(null);
+
+  const plus=(e)=>{
+    e.preventDefault();
+    setResult(result+Number(inputRef.current.value))
+  }
+
+  const minus=(e)=>{
+    e.preventDefault();
+    setResult(result-Number(inputRef.current.value))
+
+  }
+  const multiply=(e)=>{
+    e.preventDefault();
+    setResult(result*Number(inputRef.current.value))
+
+  }
+  const division=(e)=>{
+    e.preventDefault();
+    setResult(result/Number(inputRef.current.value))
+
+  }
+  const resetInput=(e)=>{
+    e.preventDefault();
+    inputRef.current.value=0;
+
+  }
+  const resetOutput=(e)=>{
+    e.preventDefault();
+    setResult(0);
+
+  }
+
+
+
+
+
+  return (
+    <form>
+
+    <h1>Basic caluculator</h1>
+    <p ref={resultRef} >{result}</p>
+    <input type="number" ref={inputRef} placeholder="enter input here"/>
+    <div>
+      <button onClick={plus}>add</button>
+      <button onClick={minus}>subtract</button>
+      <button onClick={multiply}>multiply</button>
+      <button onClick={division}>divide</button>
+      <button onClick={resetInput}>reset input</button>
+      <button onClick={resetOutput}>reset output</button>
+    </div>
+
+    </form>
+
+
+
+
+  )
+
+
+}
+
+export default App
